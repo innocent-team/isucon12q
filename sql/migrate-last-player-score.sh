b842a5938b14083c94d67bc57546cb03ee587352
utgwkk <utagawakiki@gmail.com>
2022-07-23 11:46:20 +0900

    いったんはマイグレーションだけ流し込む

--- sql/migrate-last-player-score.sh
+++ sql/migrate-last-player-score.sh
@@ -11,5 +11,5 @@ cd `dirname $0`
 
 for tenant_db in ../../initial_data/*.db; do
     sqlite3 $tenant_db < tenant/20_last_player_score.sql
-	./sqlite3-to-sql $tenant_db | mysql -u"$ISUCON_DB_USER" -p"$ISUCON_DB_PASSWORD" --host "$ISUCON_DB_HOST" --port "$ISUCON_DB_PORT" "$ISUCON_DB_NAME"
+	# ./sqlite3-to-sql $tenant_db | mysql -u"$ISUCON_DB_USER" -p"$ISUCON_DB_PASSWORD" --host "$ISUCON_DB_HOST" --port "$ISUCON_DB_PORT" "$ISUCON_DB_NAME"
 done
