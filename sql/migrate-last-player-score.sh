62f104f3be5b5f3c25fa34d5dee5dfaa05915609
utgwkk <utagawakiki@gmail.com>
2022-07-23 11:35:02 +0900

    変数とってくる

--- sql/migrate-last-player-score.sh
+++ sql/migrate-last-player-score.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -exu
 
+ISUCON_DB_HOST=${ISUCON_DB_HOST:-127.0.0.1}
+ISUCON_DB_PORT=${ISUCON_DB_PORT:-3306}
+ISUCON_DB_USER=${ISUCON_DB_USER:-isucon}
+ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
+ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
+
 cd `dirname $0`
 
 for tenant_db in ../../initial_data/*.db; do
