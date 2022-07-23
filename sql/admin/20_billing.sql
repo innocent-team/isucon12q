85ad698c55bc02733d7701f049164dada162375f
utgwkk <utagawakiki@gmail.com>
2022-07-23 13:34:24 +0900

    typo

--- sql/admin/20_billing.sql
+++ sql/admin/20_billing.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS `billing`;
 
 CREATE TABLE `billing` (
-  `tennant_id` BIGINT UNSIGNED NOT NULL,
+  `tenant_id` BIGINT UNSIGNED NOT NULL,
   `competition_id` VARCHAR(255) NOT NULL UNIQUE,
   `player_count` INT NOT NULL,
   `visit_count` INT NOT NULL
