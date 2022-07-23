efcd3bd75e3307367b724ae3bea715037eb926e5
utgwkk <utagawakiki@gmail.com>
2022-07-23 15:41:25 +0900

    player_score DBを初期化する

--- sql/init.sh
+++ sql/init.sh
@@ -8,6 +8,8 @@ ISUCON_DB_PORT=${ISUCON_DB_PORT:-3306}
 ISUCON_DB_USER=${ISUCON_DB_USER:-isucon}
 ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
 ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
+ISUCON_SCORE_DB_HOST=${ISUCON_SCORE_DB_HOST:-127.0.0.1}
+ISUCON_SCORE_DB_PORT=${ISUCON_SCORE_DB_PORT:-3306}
 
 # MySQLを初期化
 # mysql -u"$ISUCON_DB_USER" \
@@ -30,6 +32,14 @@ mysql -u"$ISUCON_DB_USER" \
 		--host "$ISUCON_DB_HOST" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < ./10_add_index.sql
+
+# player_score DB用の初期化
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_SCORE_DB_HOST" \
+		--port "$ISUCON_SCORE_DB_PORT" \
+		"$ISUCON_DB_NAME" < /home/isucon/dump/isuports.billing_visit_history_simple.dump
+
 # テナントごとの情報もMySQLに流し込む
 # ./sqlite3-to-sql ../../master.db | mysql -u"$ISUCON_DB_USER" -p"$ISUCON_DB_PASSWORD" --host "$ISUCON_DB_HOST" --port "$ISUCON_DB_PORT" "$ISUCON_DB_NAME"
 
