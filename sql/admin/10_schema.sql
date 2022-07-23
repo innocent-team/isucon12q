91a8995437ea366100ab176e0d047b49c216e739
utgwkk <utagawakiki@gmail.com>
2022-07-23 13:24:46 +0900

    おしり

--- sql/admin/10_schema.sql
+++ sql/admin/10_schema.sql
@@ -35,5 +35,5 @@ CREATE TABLE `billing` (
   `tennant_id` BIGINT UNSIGNED NOT NULL,
   `competition_id` VARCHAR(255) NOT NULL UNIQUE,
   `player_count` INT NOT NULL,
-  `visit_count` INT NOT NULL,
-)
+  `visit_count` INT NOT NULL
+);
