46737b35e1a95456266c7e25d0241e51ea90c34c
utgwkk <utagawakiki@gmail.com>
2022-07-23 13:36:18 +0900

    visitor_count

--- sql/admin/20_billing.sql
+++ sql/admin/20_billing.sql
@@ -4,5 +4,5 @@ CREATE TABLE `billing` (
   `tenant_id` BIGINT UNSIGNED NOT NULL,
   `competition_id` VARCHAR(255) NOT NULL UNIQUE,
   `player_count` INT NOT NULL,
-  `visit_count` INT NOT NULL
+  `visitor_count` INT NOT NULL
 );
