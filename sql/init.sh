d5cb2a635d51b94daa3f368b5adb071feb56553c
utgwkk <utagawakiki@gmail.com>
2022-07-23 10:59:52 +0900

    fork

--- sql/init.sh
+++ sql/init.sh
@@ -23,7 +23,7 @@ mysql -u"$ISUCON_DB_USER" \
 		"$ISUCON_DB_NAME" < tenant/10_schema.sql
 
 for tenant_db in ../../initial_data/*.db; do
-	./sqlite3-to-sql $tenant_db | mysql -u"$ISUCON_DB_USER" -p"$ISUCON_DB_PASSWORD" --host "$ISUCON_DB_HOST" --port "$ISUCON_DB_PORT" "$ISUCON_DB_NAME"
+	./sqlite3-to-sql $tenant_db | mysql -u"$ISUCON_DB_USER" -p"$ISUCON_DB_PASSWORD" --host "$ISUCON_DB_HOST" --port "$ISUCON_DB_PORT" "$ISUCON_DB_NAME" &
 done
 
 # # SQLiteのデータベースを初期化
