490086f03df53139f3a4e3da143f89d68397724c
innocent <innocent@example.com>
2022-07-23 02:46:22 +0000

    dep

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,36 @@
 
 cd `dirname $0`
 
+echo $HOSTNAME
+
+if [[ "$HOSTNAME" == isucon1 ]]; then
+  INSTANCE_NUM="1"
+elif [[ "$HOSTNAME" == isucon2 ]]; then
+  INSTANCE_NUM="2"
+elif [[ "$HOSTNAME" == isucon3 ]]; then
+  INSTANCE_NUM="3"
+else
+  echo "Invalid host"
+  exit 1
+fi
+
 set -ex
 
 git pull
 
-sudo install -o root -g root -m 644 ./conf/nginx/sites-available/isuports.conf /etc/nginx/sites-available/isuports.conf
-sudo systemctl restart nginx
-sudo systemctl enable nginx
+sudo systemctl daemon-reload
 
-sudo systemctl restart isuports.service
-sudo systemctl enable isuports.service
+# toriaezu
+if [[ "$INSTANCE_NUM" == 1 || "$INSTANCE_NUM" == 3 ]]; then
+  sudo install -o root -g root -m 644 ./conf/nginx/sites-available/isuports.conf /etc/nginx/sites-available/isuports.conf
+  sudo systemctl restart nginx
+  sudo systemctl enable nginx
+  
+  sudo systemctl restart isuports.service
+  sudo systemctl enable isuports.service
+  
+  sleep 3
+  
+  sudo systemctl status isuports.service
+fi
 
-sleep 3
-
-sudo systemctl status isuports.service
