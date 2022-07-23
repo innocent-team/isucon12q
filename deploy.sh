97e3889517a2981a569ce96a5e4e779f8a8c90ac
innocent <innocent@example.com>
2022-07-23 02:49:49 +0000

    nginx.conf

--- deploy.sh
+++ deploy.sh
@@ -24,13 +24,15 @@ sudo systemctl daemon-reload
 # toriaezu
 if [[ "$INSTANCE_NUM" == 1 || "$INSTANCE_NUM" == 3 ]]; then
   sudo install -o root -g root -m 644 ./conf/nginx/sites-available/isuports.conf /etc/nginx/sites-available/isuports.conf
+  sudo install -o root -g root -m 644 ./conf/nginx/nginx.conf /etc/nginx/nginx.conf
+
   sudo systemctl restart nginx
   sudo systemctl enable nginx
   
   sudo systemctl restart isuports.service
   sudo systemctl enable isuports.service
   
-  sleep 3
+  sleep 2
   
   sudo systemctl status isuports.service
 fi
